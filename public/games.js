bf08726615668072c32ecbbdd5b5b337452b458a
Eyüpcan Çil <eyupcann.cil@gmail.com>
2026-08-13 15:49:27 +0300

    Update games.js

--- public/games.js
+++ public/games.js
@@ -2,22 +2,18 @@ const socket = io();
 const urlParams = new URLSearchParams(window.location.search);
 const odaId = urlParams.get('oda');
 
-// Kullanıcı adı yoksa lobiye geri at
 const kullaniciAdi = sessionStorage.getItem('kullaniciAdi');
 if (!kullaniciAdi) {
   window.location.href = '/';
 }
 
-// Oyuna adımızla birlikte katılıyoruz
 socket.emit('oyuna_katil', { odaId: odaId, kullaniciAdi: kullaniciAdi });
 
-// Masa ilk yüklendiğinde ayarları (Masa kodu, Tur Sayısı) alır
 socket.on('masa_bilgisi', (data) => {
   document.getElementById('ekranMasaKodu').innerText = data.pin;
   document.getElementById('ekranTurSayisi').innerText = `1 / ${data.ayarlar.turSayisi}`;
 });
 
-// Odaya biri girdiğinde
 socket.on('oyuncu_sayisi_guncelle', (sayi) => {
   const beklemeYazisi = document.getElementById('beklemeYazisi');
   if(sayi < 4) {
@@ -25,12 +21,8 @@ socket.on('oyuncu_sayisi_guncelle', (sayi) => {
   }
 });
 
-// OYUN BAŞLADIĞINDA (4 KİŞİ OLUNCA)
 socket.on('oyun_basladi', (oyunDurumu) => {
-  // Bekleme yazısını gizle
   document.getElementById('beklemeAlani').style.display = 'none';
-  
-  // Taşlarımızı ıstakaya diz
   taslariEkranaBas(oyunDurumu.oyuncular.oyuncu1);
 });
 
